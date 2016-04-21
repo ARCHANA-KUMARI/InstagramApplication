cae999994720ac393480219f827bcf3620db097f
archana <ms.archanacusat2015@gmail.com>
2016-04-21 10:10:11 +0530

    Initial commit for stage three

--- app/src/main/java/com/robosoft/archana/instagramapplication/Activity/UserProfileActivity.java
+++ app/src/main/java/com/robosoft/archana/instagramapplication/Activity/UserProfileActivity.java
@@ -15,6 +15,8 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        initUi();
+        setUpBackButton();
 
 
 
@@ -27,8 +29,8 @@ public class UserProfileActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(menuItem);
     }
 
-    private void initUi(){
 
+    private void initUi(){
         mToolbar = (Toolbar) findViewById(R.id.toolbarofuser);
     }
     private void setUpBackButton(){
